4e8c5e8c14648edf606203f07e0fa0a91ddfc526
hoochucat <ohnanabanha@gmail.com>
2025-06-05 17:54:27 +0900

    ✨ 출금 기능 구현
    사용자가 원하는 출금금액을 입력하고 통장 잔액에서 찾아가는 기능 추가 , 잔액부족 시 출금 안 되게도 설정함

--- ATM.py
+++ ATM.py
@@ -8,6 +8,20 @@ while True: #조건이 true이면 무한 작동함
 #4를 입력하면 종료라는 출력메세지 print()를 보여주는 코드를 작성해주세요
 #개발할 때는 최소한의 자원만 쓸 수 있도록 고려를 해보는 게 좋음
 
+    #2번 출금 기능 코드
+    if num == '2':
+        withdraw_amount = int(input('출금할 금액을 입력해주세요 : '))
+        """
+        if balance >= withdraw_amount 
+            print(f'출금할 금액은 {withdraw_amount}이며, 잔액은 {balance} 입니다.')
+        else: balance -= withdraw_amount
+            print("잔액이 부족합니다. 잔액을 확인 후 시도해주시기 바랍니다."
+    
+        """
+        withdraw_amount = min(balance, withdraw_amount) #min을 이용해 잔액보다 작을 시 출금이 안 되게 할 수 있음
+        balance -= withdraw_amount
+        print(f"출금하신 금액은 {withdraw_amount}이고, 현재 잔액은 {balance}원 입니다.")
+
     #1번 입금 기능 코드
     if num == '1':
         #얼마 입금할래?
@@ -17,8 +31,7 @@ while True: #조건이 true이면 무한 작동함
         balance += deposit_amount
     print(f"입금하신 금액은 {deposit_amount}'원이고, 현재 잔액은 {balance}원 입니다.") 
 
-    #
-    
+
 
     #4번 종료 기능 코드
     if num == '4':
